bfe77441d890d99477ee62c7176d709babdcb3b8
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2024-12-15 20:52:41 +0200

    Update blueprints.py

--- server/blueprints.py
+++ server/blueprints.py
@@ -11,12 +11,6 @@ from quiz_system.quiz_service import quiz_service_api
 
 
 def register_blueprints(app):
-    app.register_blueprint(havruta_api)
-    app.register_blueprint(tremp_api)
-    app.register_blueprint(free_items_api)
-    app.register_blueprint(temporary_position_api)
-    app.register_blueprint(zmanim_api)
-    app.register_blueprint(private_teacher_api)
     app.register_blueprint(auth_api)
     app.register_blueprint(quizzes_interface_api)
     app.register_blueprint(quiz_service_api)
