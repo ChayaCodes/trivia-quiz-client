ae031e6677fe4090bc6851732b619669485ce29a
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2024-12-15 20:52:56 +0200

    Update blueprints.py

--- server/blueprints.py
+++ server/blueprints.py
@@ -1,10 +1,5 @@
 # blueprints.py
-from havruta import havruta_api
-from tremp import tremp_api
-from free_items import free_items_api
-from temporary_position import temporary_position_api
-from zmanim import zmanim_api
-from private_teacher import private_teacher_api
+
 from quiz_system.auth_api import auth_api
 from quiz_system.quizzes_interface_api import quizzes_interface_api
 from quiz_system.quiz_service import quiz_service_api
