d272d56cb41794b67233e4dc33c65e13506ba39e
MoriyaDev <moriyaprober@gmail.com>
2024-12-18 21:33:57 +0200

    Adding protection to links for registered users only

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,11 @@ import CreateQuiz from "./pages/CreateQuiz/CreateQuiz";
 import QuizPage from "./pages/QuizPage/QuizPage";
 import Quizzes from "./pages/Quizzes/Quizzes";
 import Layout from "./components/Layout/Layout"
-
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   const [userName, setUserName] = useState(null);
+  const isLoggedIn = !!userName; 
+
 
   return (
     <Router>
@@ -21,9 +23,9 @@ function App() {
           <Route path="/" element={<Home userName={userName} setUserName={setUserName} />} />
           <Route path="/login" element={<Login setUserName={setUserName} />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/create-quiz" element={<CreateQuiz />} />
+          <Route path="/create-quiz" element={<ProtectedRoute isLoggedIn={isLoggedIn}><CreateQuiz /></ProtectedRoute>} />
+          <Route path="/quizzes" element={<ProtectedRoute isLoggedIn={isLoggedIn}><Quizzes /></ProtectedRoute>} />
           <Route path="/quiz" element={<QuizPage userName={userName} />} />
-          <Route path="/quizzes" element={<Quizzes />} />
         </Routes>
       </Layout>
     </Router>
